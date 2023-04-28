f5616aea870d8938a4364623485e0f9f6a49fb2d
WinnieNH <winniehoogakker@gmail.com>
2023-04-28 17:30:40 +0200

    Collect data but no measure BOTH

--- Test274.py
+++ Test274.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
+z = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(z, np.sin(z))       # Plot the sine of each x point
 plt.show()                   # Display the plot
 
 url = "https://pokeapi.co/api/v2/pokemon/"
@@ -22,16 +22,32 @@ df.to_csv('PokeOutput.csv', encoding='utf-8', index=False)
 #print(dataCol) 
 
 x = input('search now: ')
+x = x.casefold()
 y = 0
 
 for item in dataCol["results"]:
     slect = dataCol["results"][y]
     name = slect["name"]
+    
     #print(slect)
     if x in name:
         slectPok = dataCol["results"][y]
         print(slectPok)
-          
+        url2 = "https://pokeapi.co/api/v2/pokemon/" + x
+        print(url2)
+
+        response2 = requests.get(url2)
+        jsonp2 = response2.json()
+
+        dataCol2 = json.loads(response2.text)
+        
+        """x2 = input('search now: ')
+        x2 = x2.casefold()
+        y2 = 0
+
+        slect2 = dataCol2["results"][y2]"""
+        print(dataCol2)
+        
     
     y += 1
 
--- start.py
+++ start.py
@@ -1,3 +1,28 @@
 import pandas
+import matplotlib.pyplot as plt
+import numpy as np
 
-print('test')
+mijnbestand = pandas.read_csv("Pokemon.csv", na_filter=False)
+
+z = np.linspace(0, 2)  # Create a list of evenly-spaced numbers over the range
+
+
+#print(mijnbestand.columns)
+
+x = input('search name of Pokemon or Move: ')
+
+for index, pokemon in mijnbestand.iterrows():
+    if (x.casefold() == "all"):
+        print(pokemon["Name"], pokemon["Type 1"], pokemon["Type 2"])
+    else:
+        if(x in pokemon["Name"]):
+            print(pokemon["Name"], pokemon["HP"])
+            z += pokemon["HP"]
+            
+        if (x in pokemon["Type 1"] or x in pokemon["Type 2"]):
+            print(pokemon["Name"], pokemon["Type 1"], pokemon["Type 2"])
+
+plt.plot(z, np.sin(z))       # Plot the sine of each x point
+plt.show() 
+    
+        
