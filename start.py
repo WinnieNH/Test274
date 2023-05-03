f23eafd7128f65fe9aead4570ecba9a95aea1947
WinnieNH <winniehoogakker@gmail.com>
2023-05-03 09:11:38 +0200

    cleaning up of start file

--- start.py
+++ start.py
@@ -1,23 +1,26 @@
 import pandas
 import matplotlib.pyplot as plt
 import numpy as np
-
 import random
 
 pokebestand = pandas.read_csv("Pokemon.csv", na_filter=False)
 ACbestand = pandas.read_csv("Villagers.csv", na_filter=False)
 
-z = np.linspace(0, 2)  
+z = np.linspace(0, 2)   #Waarom heb ik deze chart nog?
 
 HPcalc = []
 
-start = input('AC or Pokemon: search, stats, random or (random) fight? ')
+start = input("AC or Pokemon: search, stats, random or (random) fight? ")
+
 
 if(start == "AC"):
-    print(ACbestand["Name"])
-elif(start == "search"):
-    search = input('Show all or search name of Pokemon/Move or legendary: ')
+    print("What info? ")
+    interest = input()
+    print(ACbestand[interest])
 
+elif(start == "search"):
+    search = input("Show all or search name of Pokemon/Move or legendary: ")
+    
     for index, pokemon in pokebestand.iterrows():
         if (search.casefold() == "all"):
             print(pokemon["Name"], pokemon["Type 1"], pokemon["Type 2"])
@@ -39,6 +42,7 @@ elif(start == "search"):
 
 elif (start == 'stats'):
     totalHP = 0
+    
     for index, pokemon in pokebestand.iterrows():
         totalHP = totalHP + pokemon["HP"] #eerste berekening, wel af
         print("totalHP: ", totalHP)
@@ -47,7 +51,7 @@ elif (start == 'stats'):
         plt.hist(HPcalc)
     plt.show()    
     
-elif(start == 'random'):
+elif(start == "random"):
     from random import *
     num = randint(1, 802)
     
@@ -55,7 +59,7 @@ elif(start == 'random'):
         if (num == pokemon["#"]):
             print(pokemon["Name"])
 
-elif(start == "fight"): #TWEEDE BEREKENING DIE NOG AF MOET!
+elif(start == "fight"): 
     from random import *
     num = randint(1, 802)
     num2 = randint(1, 802)
@@ -64,6 +68,7 @@ elif(start == "fight"): #TWEEDE BEREKENING DIE NOG AF MOET!
         if(num == pokemon["#"]):
             pok1stats = pokemon["Total"]
             pok1name = pokemon["Name"]
+    
     for index, pokemon in pokebestand.iterrows():
         if (num2 == pokemon["#"]):
             pok2stats = pokemon["Total"]
@@ -73,6 +78,7 @@ elif(start == "fight"): #TWEEDE BEREKENING DIE NOG AF MOET!
 
     print(pok1name, pok1stats)
     print(pok2name, pok2stats)
+    
     if(pok1stats > pok2stats):
         print(pok1name + " wins")
     elif(pok1stats < pok2stats):
