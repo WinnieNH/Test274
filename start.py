6f2ffa94ab8d566b071b6f77cc0ad18202ea9d54
WinnieNH <winniehoogakker@gmail.com>
2023-05-03 09:32:07 +0200

    clean up start file 2

--- start.py
+++ start.py
@@ -6,8 +6,6 @@ import random
 pokebestand = pandas.read_csv("Pokemon.csv", na_filter=False)
 ACbestand = pandas.read_csv("Villagers.csv", na_filter=False)
 
-z = np.linspace(0, 2)   #Waarom heb ik deze chart nog?
-
 HPcalc = []
 
 start = input("AC or Pokemon: search, stats, random or (random) fight? ")
@@ -30,15 +28,9 @@ elif(start == "search"):
         else:
             if(search in pokemon["Name"] or search.casefold() in pokemon["Name"]):
                 print(pokemon["Name"], pokemon["HP"])
-                z += pokemon["HP"]
                 
             if (search in pokemon["Type 1"] or search in pokemon["Type 2"] or search.casefold() in pokemon["Type 1"] or search.casefold() in pokemon["Type 2"]):
                 print(pokemon["Name"], pokemon["Type 1"], pokemon["Type 2"])
-        
-        
-
-    plt.plot(z, np.sin(z))       
-    plt.show() 
 
 elif (start == 'stats'):
     totalHP = 0
